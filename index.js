2e43981a1ae3217a1cae4a8388473e2c01a57e41
mike <mikesapienzaaudio@gmail.com>
2020-10-28 16:28:39 -0500

    finished

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value){
+   const updated_driver = { ...driver }
+   updated_driver[key] = value
+   return updated_driver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
+    driver[key] = value
+    return driver
+}
+
+
+function deleteFromDriverByKey(driver,key){
+  const updated_driver = { ...driver }
+  delete updated_driver[key]
+  return updated_driver
+}
+
+function destructivelyDeleteFromDriverByKey(driver,key){
+    delete driver[key]
+    return driver
+}
